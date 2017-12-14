f9c76ca268de28a82a80f5d4671091bd7e687435
Stepan <stepan.kasyanenko@gmail.com>
2017-12-14 17:30:09 +0500

    Added include

--- fftw_test.cpp
+++ fftw_test.cpp
@@ -1,19 +1,14 @@
-#include <fftw3.h>
 #include <iostream>
+#include <keyfinder/keyfinder.h>
+#include <keyfinder/constants.h>
+#include <keyfinder/fftadapter.h>
 
 int main(void)
 {
-    std::cout<<"hello"<<"\n";
-    int N = 1024;
-    fftw_complex *in, *out;
-    fftw_plan my_plan;
-    in = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * N);
-    out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * N);
-    my_plan = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-    fftw_execute(my_plan);
-    fftw_destroy_plan(my_plan);
-    fftw_free(in);
-    fftw_free(out);
+    // KeyFinder::KeyFinder key_finder;
+    // KeyFinder::AudioData audio_data;
+    // KeyFinder::key_t key;
+    // KeyFinder::Workspace workspace;
     std::cout<<"hello 4"<<"\n";
     return 0;
 
