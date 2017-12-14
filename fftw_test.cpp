15e77d1a5b0340fce43eb02f7529caf7b48f51ba
Stepan <stepan.kasyanenko@gmail.com>
2017-12-14 18:11:58 +0500

    fix

--- fftw_test.cpp
+++ fftw_test.cpp
@@ -6,7 +6,7 @@
 int main(void)
 {
     std::cout<<"heelo 7"<<"\n";
-    KeyFinder::FftAdapter fftAdapter = new KeyFinder::FftAdapter(4);
+    KeyFinder::FftAdapter fftAdapter* = new KeyFinder::FftAdapter(4);
     fftAdapter->setInput(0,0.12);
     fftAdapter->setInput(1,0.32);
     fftAdapter->setInput(2,0.43);
