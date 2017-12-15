d432335d8140f13101f6123f5079f86a75d3455b
Stepan <stepan.kasyanenko@gmail.com>
2017-12-15 10:08:36 +0500

    Test 10 length array

--- fftw_test.cpp
+++ fftw_test.cpp
@@ -5,13 +5,25 @@
 
 int main(void)
 {
-    std::cout<<"heelo 7"<<"\n";
-    KeyFinder::FftAdapter* fftAdapter = new KeyFinder::FftAdapter(4);
-    fftAdapter->setInput(0,0.12);
-    fftAdapter->setInput(1,0.32);
-    fftAdapter->setInput(2,0.43);
-    fftAdapter->setInput(3,0.56);
+    std::cout << "heelo 7"
+              << "\n";
+    KeyFinder::FftAdapter *fftAdapter = new KeyFinder::FftAdapter(4);
+    double n[10] = {0.5057348352023452,
+                    0.5814739393180786,
+                    0.057877523258875296,
+                    0.5645083105402466,
+                    0.19422643900648517,
+                    0.33551343269112155,
+                    0.9929052686408,
+                    0.231633714006944,
+                    0.5280832258875852,
+                    0.5435797861871221};
+    for ( int j = 0; j < 10; j++ ) {
+      fftAdapter->setInput(j, n[j]);
+    }                
     fftAdapter->execute();
-    std::cout<<fftAdapter->getOutputReal(0)<<"\n";
+    for ( int j = 0; j < 10; j++ ) {
+      std::cout << fftAdapter->getOutputReal(j) << "\n"
+    } 
     return 0;
 }
