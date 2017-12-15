1fcb8fea068b6d7a6995ffeb7827835e391b85fb
Stepan <stepan.kasyanenko@gmail.com>
2017-12-15 14:57:07 +0500

    fix

--- fftw_test.cpp
+++ fftw_test.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <keyfinder/fftadapter.h>
 
+#define NUM_POINTS 64
+
 int main(void)
 {
 
   std::cout << "heelo 7"
-            << "\n";
-  int sampleLen = 10;          
-  KeyFinder::FftAdapter *fftAdapter = new KeyFinder::FftAdapter(sampleLen);
-  double n[sampleLen] = {0.5057348352023452,
+            << "\n";   
+  KeyFinder::FftAdapter *fftAdapter = new KeyFinder::FftAdapter(NUM_POINTS);
+  double n[NUM_POINTS] = {0.5057348352023452,
                   0.5814739393180786,
                   0.057877523258875296,
                   0.5645083105402466,
@@ -18,11 +19,11 @@ int main(void)
                   0.231633714006944,
                   0.5280832258875852,
                   0.5435797861871221};
-  for ( int j = 0; j < sampleLen; j++ ) {
+  for ( int j = 0; j < NUM_POINTS; j++ ) {
     fftAdapter->setInput(j, n[j]);
   }
   fftAdapter->execute();
-  for ( int j = 0; j < sampleLen; j++ ) {
+  for ( int j = 0; j < NUM_POINTS; j++ ) {
     std::cout << fftAdapter->getOutputReal(j) << "\n";
   }
   return 0;
